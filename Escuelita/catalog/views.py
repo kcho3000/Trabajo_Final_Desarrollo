e7605423c289405b708d8bf988b88324ce131300
Eder <guusanito123@gmail.com>
2022-11-28 17:08:06 -0600

    Se agregaron las imagenes y el css de los html
    Css e imagenes para las vistas

--- Escuelita/catalog/views.py
+++ Escuelita/catalog/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse, redirect
+from catalog.models import Alumnos 
+from catalog.models import Maestro 
+
 
 # Create your views here.
+
+
+
+def index(request):
+
+ return render(request, 'index.html')
+
+def inicio_sesion(request):
+
+    return render(request, 'Login.html')
+
+def register(request):
+    return render(request,'Register.html')
+
+def crearUsuario(request):
+
+    if(request.GET["contraseña"] == request.GET["contraseñar"]):
+        constrasena = request.GET["contraseña"]
+    Alumno = Alumnos(
+    Nombre = request.GET["nombre"],
+    ApellidoP = request.GET["apellidoP"],
+    ApellidoM = request.GET["apellidoM"],
+    Correo = request.GET["correo"],
+    Contraseña = constrasena
+    )
+    Maestros = Maestro(
+    Nombre = request.GET["nombre"],
+    ApellidoP = request.GET["apellidoP"],
+    ApellidoM = request.GET["apellidoM"],
+    Correo = request.GET["correo"],
+    Contraseña = constrasena
+    )
+    if(request.GET["MoA"] == "Alumno"):
+        Alumno.save()
+    else : Maestros.save()
+
+    return HttpResponse("Se creo Usuario" , constrasena)
